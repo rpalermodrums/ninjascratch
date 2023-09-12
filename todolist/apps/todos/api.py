bff945ed4caa613e156c90bb560cb38524acc9ce
Ryan Palermo <rpalermodrums@gmail.com>
2023-09-12 10:47:28 -0400

    Reorder endpoints in todo controller

--- todolist/apps/todos/api.py
+++ todolist/apps/todos/api.py
@@ -31,33 +31,19 @@ class TodosController(ControllerBase):
     def get_list_qs(user: User) -> QuerySet[TodoList]:
         return TodoList.objects.filter(owner=user)
 
+    @staticmethod
+    def get_todo_list_with_todos(list_id: int) -> TodoList:
+        return TodoList.objects.get(id=list_id).prefetch_related('todo_items')
+
     @http_get('/todos', response=List[TodoOut], by_alias=True)
     @paginate(PageNumberPagination)
-    def list_todos(self, request, list_id: int = 1) -> List[Todo]:
+    def list_todos(self, request, list_id: int) -> List[Todo]:
         todo_list = self.get_object_or_exception(self.get_qs(request.user), list__id=list_id)
         return todo_list.todo_items
-
-    @http_post('/todos', response=TodoOut, by_alias=True)
-    def create_todo(self, request, data: TodoIn) -> Todo:
-        todo_list, created = self.get_qs(request.user).get_or_create(id=getattr(data, 'list'), owner=request.user)
-        return Todo.objects.create(data.dict(), list=todo_list)
 
     @http_get('/todos/{todo_id}', response=TodoOut, by_alias=True)
     def get_todo(self, request, todo_id: int) -> Todo:
         return self.get_object_or_exception(self.get_qs(request.user), id=todo_id)
-
-    @http_post('/todos', response=TodoOut, by_alias=True)
-    def create_todo(self, _request, payload: TodoIn) -> Todo:
-        return Todo.objects.create(**payload.dict())
-
-    @http_put('/todos/{todo_id}', response=TodoOut, by_alias=True)
-    def update_todo(self, request, todo_id: int, payload: TodoIn) -> Todo:
-        todo = self.get_object_or_exception(self.get_qs(request.user), id=todo_id)
-        return update_from_payload(todo, payload)
-
-    @http_delete('/todos/{todo_id}', response=None)
-    def delete_todo(self, request, todo_id: int) -> None:
-        self.get_object_or_exception(self.get_qs(request.user), id=todo_id).delete()
 
     @http_get('/todos/lists', response=TodoListSchema, by_alias=True)
     @paginate(PageNumberPagination)
@@ -66,16 +52,29 @@ class TodosController(ControllerBase):
 
     @http_get('/todos/lists/{list_id}', response=TodoListSchema, by_alias=True)
     def get_todo_list(self, request, list_id: int) -> TodoList:
-        return self.get_object_or_exception(self.get_qs(request.user), id=list_id)
+        return self.get_todo_list_with_todos(list_id)
+
+    @http_post('/todos', response=TodoOut, by_alias=True)
+    def create_todo(self, _request, payload: TodoIn) -> Todo:
+        return Todo.objects.create(**payload.dict())
 
     @http_post('/todos/lists', response=TodoListSchema, by_alias=True)
     def create_todo_list(self, request, title: str) -> TodoList:
         return TodoList.objects.create(owner=request.user, title=title)
 
+    @http_patch('/todos/{todo_id}', response=TodoOut, by_alias=True)
+    def partial_update_todo(self, request, todo_id: int, payload: TodoIn) -> Todo:
+        todo = self.get_object_or_exception(self.get_qs(request.user), id=todo_id)
+        return update_from_payload(todo, payload)
+
     @http_patch('/todos/lists/{list_id}', response=TodoListSchema, by_alias=True)
     def partial_update_todo_list(self, request, list_id: int, payload: TodoListSchema) -> TodoList:
         todolist = self.get_object_or_exception(self.get_qs(request.user), id=list_id)
         return update_from_payload(todolist, payload)
+
+    @http_delete('/todos/{todo_id}', response=None)
+    def delete_todo(self, request, todo_id: int) -> None:
+        self.get_object_or_exception(self.get_qs(request.user), id=todo_id).delete()
 
     @http_delete('/todos/lists/{list_id}', response=None, by_alias=True)
     def delete_todo_list(self, request, list_id: int) -> None:
